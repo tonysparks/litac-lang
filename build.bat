6c86818e47489fa8564a6448391f96a3db8d58c7
tonysparks <anthony.sparks31@gmail.com>
2020-11-25 10:27:39 -0600

    get some more tests to pass!

--- build.bat
+++ build.bat
@@ -6,7 +6,7 @@ del ".\bin\litac.*" /q
 set BUILD_CMD="clang.exe -std=c99 -g -gcodeview -fsanitize=undefined,address %%input%% -o %%output%%  -D_CRT_SECURE_NO_WARNINGS -I../include -L../lib"
 java -jar %LITAC_HOME%/target/litac.jar -buildCmd %BUILD_CMD% -outputDir "./bin" -output "litaC" "./src/main.lita" -types "none" -debug
 cd bin
-litaC.exe -run -profile "../test/test3.lita"
+litaC.exe -run -profile "../test/test_single.lita"
 goto end
 
 :error_exit
