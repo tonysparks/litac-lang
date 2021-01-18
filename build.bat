8fb566b9fd862dd0bb29de912e99af43004250dc
tonysparks <anthony.sparks31@gmail.com>
2021-01-18 17:40:58 -0600

    various bug fixes

--- build.bat
+++ build.bat
@@ -4,10 +4,11 @@ if [%LITAC_HOME%] == [] goto error_exit
 del ".\bin\litac.*" /q
 
 set BUILD_CMD="clang.exe -std=c99 -g -gcodeview -fsanitize=undefined,address %%input%% -o %%output%%  -D_CRT_SECURE_NO_WARNINGS -I../include -L../lib"
-java -jar %LITAC_HOME%/target/litac.jar -buildCmd %BUILD_CMD% -outputDir "./bin" -output "litaC" "./src/main.lita" -types "none" -debug
+java -jar %LITAC_HOME%/target/litac.jar -profile -buildCmd %BUILD_CMD% -outputDir "./bin" -output "litaC" "./src/main.lita" -types "none" -debug
 cd bin
-litaC.exe -debug -profile -buildCmd %BUILD_CMD% "../src/main.lita"
-REM litaC.exe -debug -test "t.y" -run -profile "../test/test_single.lita"
+echo Running litaC...
+REM litaC.exe -debug -profile -buildCmd %BUILD_CMD% "../src/main.lita"
+litaC.exe -cFormat -types all -debug -run -profile "../test/test_single.lita"
 REM litaC.exe -run -profile -buildCmd %BUILD_CMD% "../src/lsp.lita"
 goto end
 
@@ -15,4 +16,4 @@ goto end
 echo ERROR: It appears you do not have the 'LITAC_HOME' system variable defined.  Please make sure this is set to the home directory of litac
 
 :end
-
+echo Completed.
