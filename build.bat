35ebe89d0553b172f7cbac8c716a42d13a7d07e9
tonysparks <anthony.sparks31@gmail.com>
2021-01-26 18:03:14 -0600

    supports compilation units

--- build.bat
+++ build.bat
@@ -4,16 +4,32 @@ if [%LITAC_HOME%] == [] goto error_exit
 del ".\bin\litac.*" /q
 
 set BUILD_CMD="clang.exe -std=c99 -g -gcodeview -fsanitize=undefined,address %%input%% -o %%output%%  -D_CRT_SECURE_NO_WARNINGS -I../include -L../lib"
-java -jar %LITAC_HOME%/target/litac.jar -profile -buildCmd %BUILD_CMD% -outputDir "./bin" -output "litaC" "./src/main.lita" -types "none" -debug
+java -jar %LITAC_HOME%/target/litac.jar -profile -buildCmd %BUILD_CMD% -outputDir "./bin" -output "litacc" "./src/main.lita" -types "none" -debug
+if errorlevel 1 (
+    goto error_compiling
+)
 cd bin
 echo Running litaC...
 REM litaC.exe -debug -profile -buildCmd %BUILD_CMD% "../src/main.lita"
-litaC.exe -cFormat -types all -debug -run -profile "../test/test_single.lita"
+litacc.exe -cFormat -types all -debug -run -profile "../test/test_single.lita"
 REM litaC.exe -run -profile -buildCmd %BUILD_CMD% "../src/lsp.lita"
 goto end
 
+
+:error_compiling
+echo.
+echo ==========================================
+echo ERROR: Compiling litac compiler
+echo ==========================================
+echo.
+goto end
+
 :error_exit
+echo.
+echo ==========================================
 echo ERROR: It appears you do not have the 'LITAC_HOME' system variable defined.  Please make sure this is set to the home directory of litac
+echo ==========================================
+echo.
 
 :end
 echo Completed.
--- build_test_file.bat
+++ build_test_file.bat
@@ -5,9 +5,9 @@ del ".\bin\litac.*" /q
 del ".\bin\litac_tests.*" /q
 
 set BUILD_CMD="clang.exe -std=c99 -g -gcodeview -fsanitize=undefined,address %%input%% -o %%output%%  -D_CRT_SECURE_NO_WARNINGS -I../include -L../lib"
-java -jar %LITAC_HOME%/target/litac.jar -buildCmd %BUILD_CMD% -outputDir "./bin" -output "litaC" "./src/main.lita" -types "none" -debug
+java -jar %LITAC_HOME%/target/litac.jar -buildCmd %BUILD_CMD% -outputDir "./bin" -output "litacc" "./src/main.lita" -types "none" -debug
 cd bin
-litaC.exe -debug -testFile -run -profile -buildCmd %BUILD_CMD% -output "litac_tests" "%1"
+litacc.exe -debug -testFile -run -profile -buildCmd %BUILD_CMD% -output "litac_tests" "%1"
 goto end
 
 :error_exit
