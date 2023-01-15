fbc561a9c81bcf69296bb5676e619000f2125eaa
tonysparks <anthony.sparks31@gmail.com>
2023-01-15 09:02:35 -0600

    fix error out on tests

--- build_test.bat
+++ build_test.bat
@@ -8,10 +8,19 @@ cd bin
 REM set BUILD_CMD="clang.exe -std=c99 -g -fsanitize=undefined,address %%input%% -o %%output%%  -D_CRT_SECURE_NO_WARNINGS -I../include -L../lib -llibtcc"
 set BUILD_CMD="clang.exe -std=c99 -g -gcodeview -fsanitize=undefined,address %%input%% -o %%output%%  -D_CRT_SECURE_NO_WARNINGS -I../include -L../lib"
 litac -buildCmd %BUILD_CMD% -cFormat -profile -run -srcDir "../src" -outputDir "./" -output "litac_tests" "../test/test_suite.lita" -types "none" -debug -maxMemory 1GiB
+if errorlevel 1 (
+    goto error_tests
+)
 goto end
 
 :error_exit
 echo ERROR: It appears you do not have the 'LITAC_HOME' system variable defined.  Please make sure this is set to the home directory of litac
+exit 31
+
+:error_tests
+echo ERROR: One or more tests have failed
+exit 31
+
 
 :end
-
+echo All tests have passed!
