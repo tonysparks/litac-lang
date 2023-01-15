4fe00cacac57a70377e870a041d3b1ef32155cc5
tonysparks <anthony.sparks31@gmail.com>
2023-01-14 22:59:00 -0600

    dbg

--- build.bat
+++ build.bat
@@ -12,7 +12,8 @@ set BUILD_CMD="clang.exe -march=native -mavx2 -mbmi2 -mpopcnt -mfma -mcx16 -mtun
 
 echo Running bootstrapped litaC...
 cd bootstrap
-litacc.exe -version
+.\litacc.exe -version
+.\litacc -version
 litacc.exe -cFormat -profile -buildCmd %BUILD_CMD% "../src/main.lita" -outputDir "../bin/" -output "litac" -maxMemory 1GiB
 echo after
 if errorlevel 1 (
