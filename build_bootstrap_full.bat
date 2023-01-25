b661bc20c06c6428c2d03dbfe35083fb0311826c
tonysparks <anthony.sparks31@gmail.com>
2023-01-24 18:00:58 -0600

    error handling in scripts

--- build_bootstrap_full.bat
+++ build_bootstrap_full.bat
@@ -3,15 +3,27 @@ set wd=%cd%
 echo Current working directory '%wd%'
 
 call %wd%\build_bootstrap.bat
+if errorlevel 1 (
+    goto end
+)
 
 cd %wd%
 call build.bat
+if errorlevel 1 (
+    goto end
+)
 
 cp "%wd%\bin\litac.c" "%wd%\bootstrap"
 move /y "%wd%\bootstrap\litac.c" "%wd%\bootstrap\litac_windows.c"
+if errorlevel 1 (
+    goto end
+)
 
 cd %wd%
 call %wd%\build_bootstrap.bat
+if errorlevel 1 (
+    goto end
+)
 
 :end
 cd %wd%
--- build_bootstrap_full.sh
+++ build_bootstrap_full.sh
@@ -4,14 +4,26 @@ wd=$(pwd)
 echo Current working directory ${wd}
 
 sh ${wd}/build_bootstrap.sh
+if [ $? -gt 0 ]; then
+    return 1
+fi
 
 cd ${wd}
 sh ${wd}/build.sh
+if [ $? -gt 0 ]; then
+    return 1
+fi
 
 cp -f "${wd}/bin/output/a.c" "${wd}/bootstrap/litac_linux.c"
+if [ $? -gt 0 ]; then
+    return 1
+fi
 
 cd ${wd}
 sh ${wd}/build_bootstrap.sh
+if [ $? -gt 0 ]; then
+    return 1
+fi
 
 cd ${wd}
 echo Completed.
