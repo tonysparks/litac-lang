779f6228b88580d985c6ea1ea2dd2c1f1f6ff17d
tonysparks <anthony.sparks31@gmail.com>
2023-09-23 20:39:08 -0500

    hopefully fix build?

--- build_bootstrap.sh
+++ build_bootstrap.sh
@@ -16,6 +16,20 @@ case "${unameOut}" in
     ;;
 esac
 
+error_compiling() {
+    echo ""
+    echo "=========================================="
+    echo "ERROR: Compiling litac compiler           "
+    echo "=========================================="
+    echo ""
+    exit 2;
+}
+
 cd bootstrap
 gcc -o "litacc" "${SOURCE_FILE}" -D_CRT_SECURE_NO_WARNINGS -D_DEFAULT_SOURCE -I../include -L../lib -I../stdlib/std/http/libcurl/include ${LIBS}
+if [ $? -gt 0 ]; then
+    error_compiling
+    return 1;
+fi
+
 echo "Successfully built bootstrap"
