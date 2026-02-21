c28690c7d39d164b1937d39797598f748b8bb00d
tonysparks <anthony.sparks31@gmail.com>
2026-02-21 13:28:10 -0600

    get test suite working in dynamic tests

--- build_test.sh
+++ build_test.sh
@@ -2,27 +2,6 @@
 
 set -e
 
-unameOut="$(uname -s)"
-case "${unameOut}" in
-    Darwin*)
-        LIBS="-lpthread";
-    ;;
-    *)
-        LIBS="-lm -lrt -lpthread";
-    ;;
-esac
-
-BUILD_CMD="gcc -g %input% -o %output% -D_CRT_SECURE_NO_WARNINGS -I../include -L../lib ${LIBS}"
-
-error_compiling() {
-    echo ""
-    echo "=========================================="
-    echo "ERROR: Compiling litac compiler           "
-    echo "=========================================="
-    echo ""
-    exit 1
-}
-
 error_tests() {
     echo ""
     echo "=========================================="
@@ -33,21 +12,17 @@ error_tests() {
 }
 
 run_tests() {
-    echo "Compiling litaC tests..."
-
-    cd ./bin
-    ./litac -verbose -buildCmd "${BUILD_CMD}" -cFormat -profile -srcDir "../src" -outputDir "./" -output "litac_tests" "../test/test_suite.lita" -types "none" -maxMemory 1GiB
-    result=$?
-    if [ $result -gt 0 ]; then
-        error_compiling
-    fi
-
     echo "Running litaC tests..."
     # valgrind --leak-check=full --show-leak-kinds=all ./litac_tests $1
-    ./litac_tests $1
+    # ./litac_tests $1
+    if [ -n "$1" ]; then
+        ./bin/litac test -regex "singleOnly" -file "./test/compiler_test.lita"
+    else
+        ./bin/litac test
+    fi
+
     result=$?
 
-    echo "Result: " $result
     if [ $result -gt 0 ]; then
         error_tests
     fi
