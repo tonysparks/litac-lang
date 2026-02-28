0a12d00c7ec507ad7936fa8741f6f11d79ffb23c
tonysparks <anthony.sparks31@gmail.com>
2026-02-28 16:20:36 -0600

    get formatting working

--- build.sh
+++ build.sh
@@ -27,7 +27,7 @@ build_litac() {
     echo "Running litaC inception!..."
 
     #valgrind --leak-check=full --show-leak-kinds=all ./litac -disableLine -verbose -profile -cFormat -buildCmd "${BUILD_CMD}" "../src/main.lita" -maxMemory 1GiB -outputDir "./output/"
-    ./bin/litac build -profile -debug
+    ./bin/litac build -profile -outputDir "./bin/output/" -output "litac_inception"
     if [ $? -gt 0 ]; then
         error_compiling
         return 1;
