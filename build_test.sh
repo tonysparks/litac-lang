dfe4d286a264973b2fbcb7ddb940b017cdc93918
tonysparks <anthony.sparks31@gmail.com>
2026-05-24 16:10:35 -0500

    get pg using test containers

--- build_test.sh
+++ build_test.sh
@@ -15,14 +15,27 @@ run_tests() {
     echo "Running litaC tests..."
     litac build
 
-    # valgrind --leak-check=full --show-leak-kinds=all ./litac_tests $1
-    if [ -n "$1" ]; then
-        ./bin/output/litac test -regex "singleOnly" -file "./test/compiler_test.lita" -output "litac_tests"
-    else
-        ./bin/output/litac test -output "litac_tests"
+    DOCKER_DEFINE=""
+    if docker info > /dev/null 2>&1; then
+        DOCKER_DEFINE="-DDOCKER_AVAILABLE"
     fi
 
-    result=$?
+    # valgrind --leak-check=full --show-leak-kinds=all ./litac_tests $1
+    if [ -n "$1" ]; then
+        ./bin/output/litac test $DOCKER_DEFINE -regex "singleOnly" -file "./test/compiler_test.lita" -output "litac_tests"
+        result=$?
+    else
+        ./bin/output/litac test -output "litac_tests"
+        result=$?
+
+        # Run Docker-dependent tests separately when Docker is available, because the
+        # full-suite runner compiles each file independently and doesn't propagate
+        # top-level -D defines to individual file compilations.
+        if [ -n "$DOCKER_DEFINE" ] && [ $result -eq 0 ]; then
+            ./bin/output/litac test $DOCKER_DEFINE -file "./test/std/db/db_api_test.lita" -output "litac_tests"
+            result=$?
+        fi
+    fi
 
     if [ $result -gt 0 ]; then
         error_tests
